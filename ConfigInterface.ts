a12a0ea37437c1a7975f739de0a244c974c42b5a
Hans Christian Reinl <info@drublic.de>
2018-05-22 15:30:48 +0200

    Implement PuppeteerLaunchOptionsInterface in ConfigInterface launchOptions

--- ConfigInterface.ts
+++ ConfigInterface.ts
@@ -1,5 +1,8 @@
+import PuppeteerLaunchOptionsInterface from "./PuppeteerLaunchOptionsInterface";
+
 export default interface ConfigInterface {
   basicOnly?: boolean;
   includeFilmstrip?: boolean;
   headless?: boolean;
+  launchOptions?: PuppeteerLaunchOptionsInterface;
 }
